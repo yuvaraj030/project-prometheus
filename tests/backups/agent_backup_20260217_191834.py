f85e3c912e1bb7474ea4070bc13a1cf1d443a7a3
Yuvaraj <yuvaraj030@users.noreply.github.com>
2026-04-07 11:57:43 +0530

    chore: remove templates, tests, .github, agent_modules, benchmarks folders

--- tests/backups/agent_backup_20260217_191834.py
+++
@@ -1,8 +0,0 @@
-
-class EvolutionTarget:
-    # Target for safe evolution testing.
-    def original_method(self):
-        return 'I am original'
-
-    def evolved_method(self):
-        return 'EVOLUTION SUCCESS'
